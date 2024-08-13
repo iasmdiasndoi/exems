7e7ef2b4cd9f835c2ae8744e7e4ef7ec27db9eda
iasmdiasndoi <mirsaid1829@gmail.com>
2024-08-13 18:14:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+
+    // 1) let bolish = (5 % 2)
+    //  console.log(bolish);
+
+
+
+    //2) const userRandomNuber = alert("Sizda hozir random son chiqadi")
+    // const randomnumber = Math.round(Math.random() * 10);
+
+
+    // alert(`
+    // Random  chiqqan son ${randomnumber} 
+    // `)
+
+
+    // 3) alert(Math.floor(12.500));
+
+
+    // 4)function sozla(word) {
+    //     alert(word.length);d
+    // }
+
+    // sozla("MARS IT SCHOOL")
+
+
+    // 5) function marsit() {
+    //     for (let i = 0; i < 10; i++) {
+    //         console.log("Mars It School");
+    //     }
+    // }
+    // marsit();
+
+
+    
+// 6) let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z"];
+
+// let mirsaid = "mirsaid";
+
+// let ism = mirsaid.split('').every(harf => harflar.includes(harf));
+
+// if (ism) {
+//   console.log(mirsaid);
+// } else {
+//   console.log("qanaqdur harflar mavjud emas");
+// }
+
+
+// 7) let yosh = prompt("yoshingizni kiritng")
+
+
+
+// 8) if (yosh > 18) {
+//     console.log("balogot yoshiga etgansiz");
+// } else if (yosh == 18){
+//     console.log("balogat yoshi muborak");
+//  }else  {
+//     console.log("balogat yoshiga yetmagansiz");
+//  }
+
+
+// 10) let teskarism = prompt("Ismingizni kiriting").split("").reverse().join("");
+// console.log(teskari);
+
+// let sonlarjufttoq = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < sonlarjufttoq.length; i++) {
+//     const e = sonlarjufttoq[i]
+//     if (e % 2 === 0) {
+//         console.log(e);
+//     }
+// }
